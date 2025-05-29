8f823a6517b9427dfc3f0f99d5931473c2a0f974
Thiago Luza <thiagoluza07@gmail.com>
2025-05-29 11:48:38 -0300

    HTML e CSS

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -55,3 +55,12 @@ btnAnterior.addEventListener('click', () => {
 
 // Inicialização
 mostrarSlide(slideAtual);
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('shrink');
+  } else {
+    header.classList.remove('shrink');
+  }
+});
